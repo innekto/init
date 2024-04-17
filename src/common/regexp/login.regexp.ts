c68b841f6afc46e0a3c888ec1072ec51bd1ac62a
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2024-04-17 19:31:39 +0300

    add team form regexp

--- src/common/regexp/login.regexp.ts
+++ src/common/regexp/login.regexp.ts
@@ -4,3 +4,5 @@ export const passwordRegex = /^(?=.*[A-Z])(?=.*\d)/;
 
 export const userNameRegex =
   /^[A-Z0-9А-Я!@#\$%^&*_+\-=~?][A-Za-z0-9А-Яа-я!@#\$%^&*_+\-=~?]*$/;
+
+export const formEmailRegex = /^[a-zA-Z0-9._+-]@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
--- src/team-form/dto/create-team-form.dto.ts
+++ src/team-form/dto/create-team-form.dto.ts
@@ -1,25 +1,41 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { formEmailRegex } from 'src/common/regexp/login.regexp';
 
 export class CreateTeamFormDto {
   @ApiProperty({ example: 'Name', description: 'name' })
   @IsNotEmpty()
   @IsString()
+  @MinLength(3)
+  @MaxLength(20)
   name: string;
 
   @ApiProperty({ example: 'email@.com', description: 'email' })
   @IsNotEmpty()
   @IsEmail()
+  @Matches(formEmailRegex, { message: 'Invalid format of email' })
+  @MinLength(17)
+  @MaxLength(72)
   email: string;
 
   @ApiProperty({ example: 'Designer', description: 'position' })
   @IsNotEmpty()
   @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   position: string;
 
   @ApiProperty({ example: 'text.....', description: 'description' })
   @IsNotEmpty()
   @IsString()
+  @MaxLength(1500)
   description: string;
 
   @ApiProperty({
