108d6436756eeaf39f8dbd1283c998ae33abeba0
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2024-04-21 13:51:07 +0300

    update images

--- src/database/service/data-creation/images.ts
+++ src/database/service/data-creation/images.ts
@@ -47,22 +47,22 @@ export const whatIsDoneImages: IImages[] = [
 export const whoWeAreImages: IImages[] = [
   {
     imagePath:
-      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1712081448/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2024-01-12_%D0%B2_10.48_1_1_r9q83z.jpg',
+      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1713696351/%D0%BC%D0%B8_%D0%BF%D1%80%D0%BE%D1%84%D1%96_ghxq3l.jpg',
     description: 'cats',
   },
   {
     imagePath:
-      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1712081448/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2024-01-12_%D0%B2_10.49_1_tljdnr.jpg',
+      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1713696351/%D0%BC%D0%B8_%D0%BF%D1%80%D0%B0%D1%86%D1%8E%D1%94%D0%BC%D0%BE_cge3wk.jpg',
     description: 'cat',
   },
   {
     imagePath:
-      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1712081447/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2024-01-15_%D0%B2_14.04_1_libana.jpg',
+      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1713696351/%D0%BC%D0%B8_%D0%BA%D1%80%D0%B5%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D1%96_vcjblv.jpg',
     description: 'parrot1',
   },
   {
     imagePath:
-      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1712081448/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2024-01-15_%D0%B2_14.06_1_xtkdm0.jpg',
+      'https://res.cloudinary.com/dmbz99tlq/image/upload/v1713696351/%D0%86%D0%B7_%D0%BD%D0%B0%D0%BC%D0%B8_%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE_srpupo.jpg',
     description: 'parrot2',
   },
 ];
